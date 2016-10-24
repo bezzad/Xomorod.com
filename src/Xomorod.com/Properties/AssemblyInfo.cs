718d189bb98dc4430c94f8183c010b05b2fe226e
behzad khosravifar <behzadkh@hotmail.com>
2016-10-24 16:24:26 +0330

    update version

--- src/Xomorod.com/Properties/AssemblyInfo.cs
+++ src/Xomorod.com/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.12.31")]
-[assembly: AssemblyFileVersion("2.12.31")]
+[assembly: AssemblyVersion("2.12.32")]
+[assembly: AssemblyFileVersion("2.12.32")]
