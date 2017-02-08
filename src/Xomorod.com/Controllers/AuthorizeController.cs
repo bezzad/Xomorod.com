78b7d20deec6683156d9bcf37898273f5f68786c
Behzad Khosravifar <Behzad.khosravifar@gmail.com>
2017-02-09 00:53:38 +0330

    Fix server addresses

--- src/Xomorod.com/Controllers/AuthorizeController.cs
+++ src/Xomorod.com/Controllers/AuthorizeController.cs
@@ -1,10 +1,4 @@
 ﻿using Dapper;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Globalization;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
